0f118e63ee08a66412c539673a5d76100f8ed297
zhangshuai <cheungdaven@gmail.com>
2019-05-23 12:27:38 +1000

    swap nodes in pairs

---
+++ 24.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        pHead = ListNode(None)
+        pre, pre.next = pHead, head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return pHead.next
